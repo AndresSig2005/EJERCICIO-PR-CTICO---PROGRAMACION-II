fd9a27de879e38fe70c2dab66dfa370f9b66cf63
AndresSig2005 <andres.sigcha@udla.edu.ec>
2025-05-25 07:39:45 -0500

    Add files via upload

---
+++ Cliente.java
@@ -0,0 +1,12 @@
+public class Cliente {
+    private String nombre;
+
+    public Cliente(String nombre) {
+        this.nombre = nombre;
+    }
+
+    // Getter
+    public String getNombre() {
+        return nombre;
+    }
+}
